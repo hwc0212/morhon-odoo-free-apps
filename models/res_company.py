4a4e1778a23dbc6439bd2a7a95e8bb69b6da72d7
zj <50455729+zjkk123@users.noreply.github.com>
2021-01-26 09:31:17 +0800

    Delete models directory

--- models/res_company.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-class Company(models.Model):
-
-    _inherit = 'res.company'
-
-    company_domain = fields.Char(string="Domain", store=True)
-
